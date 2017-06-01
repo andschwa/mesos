30812e22ce65284177939afe45406443fd62a37c
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2017-05-31 22:37:48 -0700

    Windows: Fixed curl build.
    Instead of calling `msbuild` with hard-coded arguments, this instead uses the
    forwarded CMake arguments, and automatic `ExternalProject_Add` build.
    
    The `-DCURL_STATIC_LIB` must be defined here so that every target which includes
    `glog.h` has the proper configuration.

--- cmake/CompilationConfigure.cmake
+++ cmake/CompilationConfigure.cmake
@@ -220,8 +220,11 @@ if (WIN32)
 
   # COFF/PE and friends are somewhat limited in the number of sections they
   # allow for an object file. We use this to avoid those problems.
-  set(CMAKE_CXX_FLAGS
-    "${CMAKE_CXX_FLAGS} /bigobj -DGOOGLE_GLOG_DLL_DECL= -DCURL_STATICLIB /vd2")
+  string(APPEND CMAKE_CXX_FLAGS " /bigobj -DGOOGLE_GLOG_DLL_DECL= /vd2")
+
+  # TODO(andschwa): Define this closer to its usage; anything that includes
+  # `curl.h` has to set this so that the declspec is correct.
+  string(APPEND CMAKE_CXX_FLAGS " -DCURL_STATICLIB")
 
   # Enable multi-threaded compilation.
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
